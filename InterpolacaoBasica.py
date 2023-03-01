a9da1e78ba683d0cda57a0ec310c8a86c47c73d4
renanfelipelopes <renanlopes.ads@gmail.com>
2023-02-28 21:30:23 -0300

    InterpolacaoDeHexadecimal

--- InterpolacaoBasica.py
+++ InterpolacaoBasica.py
@@ -3,7 +3,7 @@ Interpolação básica de strings
 s - string
 d e i - int
 f - float
-x e x - hexadecimal (ABCDEF0123456789)
+x e X - hexadecimal (ABCDEF0123456789)
 """
 
 nome = 'Nintendo'
@@ -11,4 +11,5 @@ preco = 2250.300
 variavel = 'O preço do %s é de R$%.2f' % (nome, preco)
 
 print(variavel)
+print('O hexadecimal de %d é %04x' % (255, 255))
 

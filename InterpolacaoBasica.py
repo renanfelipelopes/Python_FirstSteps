506e55ff90cbfc42cd5d665989f2b6c0e07e4b98
renanfelipelopes <renanlopes.ads@gmail.com>
2023-02-28 21:26:27 -0300

    InterpolacaoBasica

---
+++ InterpolacaoBasica.py
@@ -0,0 +1,14 @@
+"""
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e x - hexadecimal (ABCDEF0123456789)
+"""
+
+nome = 'Nintendo'
+preco = 2250.300
+variavel = 'O preço do %s é de R$%.2f' % (nome, preco)
+
+print(variavel)
+

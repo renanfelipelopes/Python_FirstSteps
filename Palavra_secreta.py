872eda984cea6db4b267527d9316531176df6976
renanfelipelopes <renanlopes.ads@gmail.com>
2023-03-14 22:27:25 -0300

    Finalizando exercicio

--- Palavra_secreta.py
+++ Palavra_secreta.py
@@ -1,18 +1,52 @@
+import os
+
 palavra_secreta = input('Digite a palavra secreta: ')
 letras_acertadas = ''
+tentativas = 0
 
 while True:
-    letra_digitada = input('Letra digitada: ')
 
-    if len(letra_digitada > 1):
+    letra_digitada = input('Digite uma letra: ')
+    tentativas += 1
+
+    if len(letra_digitada) > 1:
         print('Digite apenas uma letra')
         continue
     
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
 
+    palavra_formada = ''
     for letra_secreta in palavra_secreta:
         if letra_secreta in letras_acertadas:
-            print(letra_secreta)
+            palavra_formada += letra_secreta
         else:
-            print('*')
+            palavra_formada += '*'
+
+    print('Palavra formada:', palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        os.system('cls')
+        print('VOCÊ GANHOU!!! PARABÉNS!')
+        print('A palavra é: ', palavra_formada)
+        print('Tentativas: ', tentativas)
+        letras_acertadas = ''
+        tentativas = 0
+
+
+"""
+Faça um jogo para o usuário adivinhar qual
+a palavra secreta.
+- Você vai propor uma palavra secreta
+qualquer e vai dar a possibilidade para
+o usuário digitar apenas uma letra.
+- Quando o usuário digitar uma letra, você 
+vai conferir se a letra digitada está
+na palavra secreta.
+    - Se a letra digitada estiver na
+    palavra secreta; exiba a letra;
+    - Se a letra digitada não estiver
+    na palavra secreta; exiba *.
+Faça a contagem de tentativas do seu
+usuário.
+"""
